44f0a607679a0969949450fafd463c973848dda4
Tanutdech <97420542+Tanutdech@users.noreply.github.com>
2022-01-10 10:56:49 +0700

    Add files via upload

---
+++ Hybrid VBS.bat
@@ -0,0 +1,20 @@
+::' ---------- Batch portion -------------------
+  rem^ &@echo off
+  rem^ &cls
+  rem^ &cscript //nologo //e:vbscript %0
+  rem^ &pause
+  rem^ &goto :eof
+
+'--------------- VBS portion ------------------
+  wscript.echo("This is a message form VBScript")
+
+
+
+
+
+
+
+
+
+
+
